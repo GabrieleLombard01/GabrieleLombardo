797fe445a7351dd69af14ba61c10db54056083df
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-15 16:19:19 +0200

    cv connect with data

--- app/Http/Controllers/Guest/CurriculumShowCaseController.php
+++ app/Http/Controllers/Guest/CurriculumShowCaseController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Experience;
+use App\Models\Instruction;
+use App\Models\Skills;
 use Illuminate\Http\Request;
 
 class CurriculumShowCaseController extends Controller
 {
     public function index()
     {
-        return view('guest.CvShowCase');
+        $experiences = Experience::orderBy('created_at')->get();
+        $instructions = Instruction::orderBy('created_at')->get();
+        $skills = Skills::orderBy('created_at')->get();
+
+        // Passa i dati alla vista utilizzando un array associativo
+        return view('guest.CvShowCase', [
+            'experiences' => $experiences,
+            'instructions' => $instructions,
+            'skills' => $skills,
+        ]);
     }
 }
