638fc3bb9995d23b8c3058bb9da88a8a9a64ccc9
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-15 16:17:38 +0200

    home and project controller connect with data

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Experience;
+use App\Models\Instruction;
+use App\Models\Project;
+use App\Models\Skills;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('guest.home');
+        $projects = Project::orderBy('created_at')->get();
+        $experiences = Experience::orderBy('created_at')->get();
+        $instructions = Instruction::orderBy('created_at')->get();
+        $skills = Skills::orderBy('created_at')->get();
+
+        // Passa i dati alla vista utilizzando un array associativo
+        return view('guest.home', [
+            'projects' => $projects,
+            'experiences' => $experiences,
+            'instructions' => $instructions,
+            'skills' => $skills,
+        ]);
     }
 }
--- app/Http/Controllers/Guest/ProjectShowCaseController.php
+++ app/Http/Controllers/Guest/ProjectShowCaseController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectShowCaseController extends Controller
 {
     public function index()
     {
-        return view('guest.ProjectShowCase');
+        $projects = Project::orderBy('created_at')->get();
+        return view('guest.ProjectShowCase', $projects);
     }
 }
