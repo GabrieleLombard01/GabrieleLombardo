ebe8bd7a2bbfea09bc22d51e23b71df3b2463a9d
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-17 19:11:35 +0200

    form add to create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -11,4 +11,67 @@
     </div>
 
 
+    <form method="POST" action="{{ route('admin.projects.store') }}">
+        @csrf
+        <div class="card_show">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12 col-md-6">
+                        <label for="title" class="mt-3 form-label">Titolo</label>
+                        <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}"
+                            placeholder="Inserisci il titolo..." max-lenght="50" required>
+                    </div>
+
+                    <div class="col-12 col-md-6">
+                        <label for="slug" class="mt-3 form-label">Slug</label>
+                        <input type="text" class="form-control" id="slug" name="slug" value="{{ old('slug') }}"
+                            max-lenght="50" disabled>
+                    </div>
+
+                    <div class="col-12">
+                        <label for="content" class="mt-3 form-label">Descrizione</label>
+                        <textarea class="form-control" id="content" name="content" rows="5" required>{{ old('content') }}</textarea>
+                    </div>
+
+
+                    <div class="col-12 col-md-10">
+                        <label for="image" class="mt-3 form-label">Immagine</label>
+                        <input type="url" class="form-control" id="image" name="image" value="{{ old('image') }}"
+                            placeholder="Inserisci un url..." max-lenght="50">
+                    </div>
+
+                    <div class="col-12 col-md-2">
+                        <div class="mt-4 d-flex justify-content-center align-items-center">
+                            <img src="{{ old('image', 'https://marcolanci.it/utils/placeholder.jpg') }}" alt="preview"
+                                class="img-fluid rounded-2 shadow w-75" id="img-preview">
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div class="d-flex justify-content-center mt-3">
+                    <button class="btn btn-success ">
+                        <i class="fas fa-floppy-disk me-2"></i> Salva
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
+    </form>
+@endsection
+
+@section('scripts')
+    <script>
+        const placeholder = 'https://marcolanci.it/utils/placeholder.jpg'
+        const imageField = document.getElementById('image');
+        const previewField = document.getElementById('img-preview');
+
+        imageField.addEventListener('input', () => {
+            previewField.src = imageField.value || placeholder;
+        });
+    </script>
 @endsection
