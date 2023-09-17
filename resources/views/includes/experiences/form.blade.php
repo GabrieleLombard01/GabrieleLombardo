fa7a345da7235d88bb80b95f1f1d78d22e33bcb4
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-17 19:38:14 +0200

    form exp fixed

--- resources/views/includes/experiences/form.blade.php
+++ resources/views/includes/experiences/form.blade.php
@@ -11,9 +11,22 @@
                 </div>
 
                 <div class="col-12 col-md-6">
-                    <label for="slug" class="mt-3 form-label">Slug</label>
-                    <input type="text" class="form-control" id="slug" name="slug" value="{{ old('slug') }}"
-                        max-lenght="50" disabled>
+                    <label for="qualification" class="mt-3 form-label">Qualifica</label>
+                    <input type="text" class="form-control" id="qualification" name="qualification"
+                        value="{{ old('qualification') }}" placeholder="Inserisci una qualifica..." max-lenght="50"
+                        required>
+                </div>
+
+                <div class="col-12 col-md-6">
+                    <label for="contract" class="mt-3 form-label">Contratto</label>
+                    <input type="text" class="form-control" id="contract" name="contract"
+                        value="{{ old('contract') }}" placeholder="Inserisci un contratto..." max-lenght="50" required>
+                </div>
+
+                <div class="col-12 col-md-6">
+                    <label for="location" class="mt-3 form-label">Luogo</label>
+                    <input type="text" class="form-control" id="location" name="location"
+                        value="{{ old('location') }}" placeholder="Inserisci il luogo..." max-lenght="50" required>
                 </div>
 
                 <div class="col-12">
@@ -21,6 +34,17 @@
                     <textarea class="form-control" id="content" name="content" rows="5" required>{{ old('content') }}</textarea>
                 </div>
 
+                <div class="col-12 col-md-6">
+                    <label for="start_date" class="mt-3 form-label">Inizio</label>
+                    <input type="date" class="form-control" id="start_date" name="start_date"
+                        value="{{ old('start_date') }}" max-lenght="50" required>
+                </div>
+
+                <div class="col-12 col-md-6">
+                    <label for="end_date" class="mt-3 form-label">Fine</label>
+                    <input type="date" class="form-control" id="end_date" name="end_date"
+                        value="{{ old('end_date') }}" max-lenght="50" required>
+                </div>
 
                 <div class="col-12 col-md-10">
                     <label for="image" class="mt-3 form-label">Immagine</label>
