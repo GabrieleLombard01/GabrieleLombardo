1eca9bc700ac097ae01daedbcb4128e141f33596
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-18 16:07:20 +0200

    edit for project add

---
+++ resources/views/admin/projects/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'Create-Project')
+
+@section('content')
+    <div class="container">
+        <div class="mt-4 mb-5 d-flex justify-content-center align-items-center">
+            <a class="btn btn-outline-primary text-decoration-none fw-bold" href="{{ url('admin/projects') }}"><i
+                    class="fa-solid fa-circle-chevron-left me-2"></i>Torna indietro</a>
+        </div>
+    </div>
+
+    @include('includes.projects.form')
+
+@endsection
+
+@section('scripts')
+    @vite('resources/js/image-previewer.js')
+    @vite('resources/js/slug-generator.js')
+@endsection
